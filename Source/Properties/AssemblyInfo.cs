258aeb04be9d1b5518d4fda3b2e583f52e5e71c1
Lamont Granquist <lamont@scriptkiddie.org>
2026-04-18 17:33:06 -0700

    Wire up KSPBurst correctly
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PatchedConicFixes")]
+[assembly: AssemblyProduct("PatchedConicFixes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("D0503AAD-78A1-4962-BA59-5FB2F384C0E3")]
+[assembly: AssemblyVersion("0.1.2.0")]
+[assembly: AssemblyFileVersion("0.1.2.0")]
+
+// Declare dependency on KSPBurst. Version 0,0 means any version is acceptable.
+[assembly: KSPAssemblyDependency("KSPBurst", 0, 0)]
